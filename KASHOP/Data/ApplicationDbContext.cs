8e0ea8758aba910f714d0860e41b274932a17f87
Tala Damery <ta.damery2@gmail.com>
2026-01-20 20:41:58 +0200

    connect with database

---
+++ KASHOP/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace KASHOP.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Data Source=.;Database=KASHOP;Integrated Security=True;Connect Timeout=30;Encrypt=True;TrustServerCertificate=True;Command Timeout=30"
+            );
+        }
+    }
+}
