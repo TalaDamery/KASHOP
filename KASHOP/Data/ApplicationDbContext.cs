d0b473b1ac2da729b8163e8e50c2c73d609e3002
Tala Damery <ta.damery2@gmail.com>
2026-01-22 23:24:07 +0200

    add admin dashboard

--- KASHOP/Data/ApplicationDbContext.cs
+++ KASHOP/Data/ApplicationDbContext.cs
@@ -7,8 +7,12 @@ namespace KASHOP.Data
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
-            optionsBuilder.UseSqlServer("Data Source=.;Database=KASHOP;Integrated Security=True;Connect Timeout=30;Encrypt=True;TrustServerCertificate=True;Command Timeout=30"
-            );
+
+            //database for development
+            //optionsBuilder.UseSqlServer("Data Source=.;Database=KASHOP;Integrated Security=True;Connect Timeout=30;Encrypt=True;TrustServerCertificate=True;Command Timeout=30");
+
+            //database for production
+            optionsBuilder.UseSqlServer("Server=db38655.public.databaseasp.net; Database=db38655; User Id=db38655; Password=A_k7y4#XKo8+; Encrypt=True; TrustServerCertificate=True; MultipleActiveResultSets=True; ");
         }
     }
 }
